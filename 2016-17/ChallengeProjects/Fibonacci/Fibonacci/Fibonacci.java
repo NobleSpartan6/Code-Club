d34f22c37422f314591403b94ca6b18e2eee7cb5
Noble <ebenezereshetu@gmail.com>
2019-11-22 14:37:04 -0600

    Update Fibonacci.java
    new comments for java file

--- 2016-17/ChallengeProjects/Fibonacci/Fibonacci/Fibonacci.java
+++ 2016-17/ChallengeProjects/Fibonacci/Fibonacci/Fibonacci.java
@@ -44,22 +44,27 @@ class Fibonacci {
    public class Fibonacci {
       public static int sizeOfSequence = 20;
       public static void main(String[] args) {
-
+  //for loop in main function returns the values from the getFib method
         for (int i = 0; i < sizeOfSequence; i++) {
           System.out.println(getFib(i));
         }
       }
+     //
       public static int getFib(int n) {
-        // get a number at position n in the sequence here
-          int[] array = new int[sizeOfSequence];
-            array[1] = 1;
-             array[2] = 1;
-
-          for (int i =3; i<sizeOfSequence; i++)
+        // get a number at position n in the sequence here  
+        int[] array = new int[sizeOfSequence];
+        //creating a new array with the size of 20
+            array[0] = 0;
+        //first element in fib sequence is 0
+             array[1] = 1;
+        //second element in fib sequence is 1
+          for (int i =2; i<=sizeOfSequence; i++)
           {
+            //use a for loop to create an array that sets each index i equal to y(n-1) + y(n-2)
             array[i] = array[i-1] + array[i-2];
           }
        return array[n];
+        //returns the array value of index n
       }
 
     }
