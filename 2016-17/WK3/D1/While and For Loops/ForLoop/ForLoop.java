2dd2d833bfa62943df25db75659ce39c0b10ad65
jacquesmmill <58564454+jacquesmmill@users.noreply.github.com>
2019-12-05 10:32:14 -0600

    Update ForLoop.java

--- 2016-17/WK3/D1/While and For Loops/ForLoop/ForLoop.java
+++ 2016-17/WK3/D1/While and For Loops/ForLoop/ForLoop.java
@@ -1,9 +1,13 @@
-// Max Omdal
-// Code Club 2016
+// Jacques Miller
+// Code Club 2019
 
 class ForLoop {
   public static void main(String[] args) {
     for (int i = 0; i < 10; i++) {
+    // For Loops count numerically
+      // This For Loop lists from 1 to 10
+      //  (Declare For Loop integer, How many times the loop will go for, change in for loop each run)
+    {
       System.out.println(i);
     }
   }
