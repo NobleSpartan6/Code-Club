86cdecd4787136d82c0738716858e526a8dbc62c
jacquesmmill <58564454+jacquesmmill@users.noreply.github.com>
2019-12-05 10:47:39 -0600

    Create WhileLoop.java

---
+++ 2016-17/WK3/D1/While and For Loops/ForLoop/WhileLoop.java
@@ -0,0 +1,12 @@
+// Code Club 2019
+// Jacques Miller
+
+class WhileLoop {
+  public static void main(String[] args) {
+    int x = 0;
+    while(x < 10) {
+      System.out.println(x);
+      x++;
+    }
+  }
+}
