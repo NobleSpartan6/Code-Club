05ee0f0226d79a00b545916bd93cf4d35b5cb907
jacquesmmill <58564454+jacquesmmill@users.noreply.github.com>
2019-12-05 10:43:43 -0600

    stuff

---
+++ 2016-17/WK3/D1/While and For Loops/ForLoop/IfThisThenThat.java
@@ -0,0 +1,23 @@
+// Code Club 2019
+// Jacques Miller
+
+public class IfThisThenThat {
+  public static void main(String[] args) {
+    Boolean This = false;
+
+if (This = true) {
+// if statements require a boolean, parameters will always equal true or false
+   WorldPeace();
+   // "This" is currently false, so the if statement is false, WorldPeace() will not run.
+}
+
+if (This = false) {
+  Return That();
+  // if statement is true, so That() will run.
+}
+
+
+}
+}
+
+// Always make sure to keep track of your brackets.
