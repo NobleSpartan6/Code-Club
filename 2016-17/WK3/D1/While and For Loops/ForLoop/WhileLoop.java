f9653d1a83b8568274b78bed2534d6ceb6f18e21
jacquesmmill <58564454+jacquesmmill@users.noreply.github.com>
2019-12-05 10:52:29 -0600

    Update WhileLoop.java

--- 2016-17/WK3/D1/While and For Loops/ForLoop/WhileLoop.java
+++ 2016-17/WK3/D1/While and For Loops/ForLoop/WhileLoop.java
@@ -5,8 +5,11 @@ class WhileLoop {
   public static void main(String[] args) {
     int x = 0;
     while(x < 10) {
+      //While Loops run until parameter is false.
+      // X is currently <10, so while will continue to run.
       System.out.println(x);
       x++;
+      //This While Loop will print 10 numbers, counting 1-10.
     }
   }
 }
